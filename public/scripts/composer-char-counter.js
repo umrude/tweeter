aab84acda2748ee823ab863e9fe588ebf867ac15
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-08 03:43:43 +0000

    add functionality for character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  let counter = document.getElementsByClassName("counter")[0].innerHTML;
+  $("textarea").keyup(() => {
+    let textLength = $('textarea').val().length;
+    let textRemaining = counter - textLength;
+    let count = $(this).find('.counter');
+    if (textRemaining >= 0) {
+      count.html(textRemaining).addClass('grey');
+    } else if (textRemaining < 0) {
+      count.html(textRemaining).removeClass('grey');
+      count.html(textRemaining).addClass('red');
+    }
+  });
+
+});
