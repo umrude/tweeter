d3644b190dc507793e2b032f5e279f75f289ef42
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-08 22:28:41 +0000

    able to fetch tweets with ajax and display on page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,31 +5,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 const createTweetElement = (data) => {
   let avatar = data.user['avatars'];
@@ -71,10 +46,21 @@ const renderTweets = function(tweets) {
   }
 };
 
+const loadTweets = function() {
+  $.ajax('/tweets', {
+    method: 'GET',
+    success: function(data) {
+      renderTweets(data);
+    }
+  });
+};
+
+
 $(document).ready(function() {
   // api calls
-  renderTweets(data);
+  loadTweets();
   //AJAX handler
+  
   $("form").on("submit", function(event) {
     event.preventDefault();
     $.ajax({
